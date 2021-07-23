f1348d220ced847e238df002c5c2ac0d22beab41
jinzhengshuai <jonathan942499655@gmail.com>
2021-07-23 14:38:03 +0800

    init map

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-  <a class="skiplink" href="#map">Go to map</a>
-  <div id="map" class="map" tabindex="0"></div>
-  <button id="zoom-out">Zoom out</button>
-  <button id="zoom-in">Zoom in</button>
+  <div id="map" class="map"></div>
+  <form class="form-inline">
+    <label for="type">Geometry type &nbsp;</label>
+    <select id="type">
+      <option value="Point">Point</option>
+      <option value="LineString">LineString</option>
+      <option value="Polygon">Polygon</option>
+      <option value="Circle">Circle</option>
+    </select>
+  </form>
   </div>
 </template>
 
 <script>
 
-
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-import View from "ol/View";
 
 export default {
   data() {
@@ -21,23 +23,11 @@ export default {
     }
   },
   created() {
-    this.init();
+
   },
   methods : {
-    init(){
-      const map = new Map({
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-        ],
-        target: 'map',
-        view: new View({
-          center: [0, 0],
-          zoom: 2,
-        }),
-      });
-    }
+
+
   }
 }
 </script>
@@ -45,25 +35,6 @@ export default {
 <style scoped>
 .map {
   width: 100%;
-  height: 400px;
-}
-a.skiplink {
-  position: absolute;
-  clip: rect(1px, 1px, 1px, 1px);
-  padding: 0;
-  border: 0;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-}
-a.skiplink:focus {
-  clip: auto;
-  height: auto;
-  width: auto;
-  background-color: #fff;
-  padding: 0.3em;
-}
-#map:focus {
-  outline: #4A74A8 solid 0.15em;
+  height:400px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'ol/ol.css';
+import 'ol/ol.css';
 import Map from 'ol/Map';
-import OSM from 'ol/source/OSM';
-import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Draw, Modify, Snap} from 'ol/interaction';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 
 
 Vue.config.productionTip = false
@@ -14,3 +17,63 @@ new Vue({
   router,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource();
+const vector = new VectorLayer({
+  source: source,
+  style: new Style({
+    fill: new Fill({
+      color: 'rgba(255, 255, 255, 0.2)',
+    }),
+    stroke: new Stroke({
+      color: '#ffcc33',
+      width: 2,
+    }),
+    image: new CircleStyle({
+      radius: 7,
+      fill: new Fill({
+        color: '#ffcc33',
+      }),
+    }),
+  }),
+});
+
+const map = new Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new View({
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
+});
+
+const modify = new Modify({source: source});
+map.addInteraction(modify);
+
+let draw, snap; // global so we can remove them later
+const typeSelect = document.getElementById('type');
+
+function addInteractions() {
+  draw = new Draw({
+    source: source,
+    type: typeSelect.value,
+  });
+  map.addInteraction(draw);
+  snap = new Snap({source: source});
+  map.addInteraction(snap);
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  map.removeInteraction(snap);
+  addInteractions();
+};
+
+addInteractions();
